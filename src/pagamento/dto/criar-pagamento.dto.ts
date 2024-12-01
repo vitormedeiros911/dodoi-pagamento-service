6422e8e3323c1cc538e57f568ed6260ff100728e
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-12-01 08:54:19 -0300

    feat: Adiciona redução de estoque

--- src/pagamento/dto/criar-pagamento.dto.ts
+++ src/pagamento/dto/criar-pagamento.dto.ts
@@ -2,6 +2,8 @@ import { IEndereco } from '../../shared/interfaces/endereco.interface';
 
 interface ItemPedidoDto {
   idProduto: string;
+  nomeProduto: string;
+  urlImagem: string;
   quantidade: number;
   precoUnitario: number;
 }
