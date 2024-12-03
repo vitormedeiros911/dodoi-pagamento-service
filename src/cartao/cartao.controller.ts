1eded25d31cf5bdbafea6661d8be711b8b12326e
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-12-03 10:14:05 -0300

    refactor: Adiciona message acknowledgements

--- src/cartao/cartao.controller.ts
+++ src/cartao/cartao.controller.ts
@@ -1,19 +1,45 @@
 import { Controller } from '@nestjs/common';
-import { EventPattern, MessagePattern } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  RmqContext,
+} from '@nestjs/microservices';
 
 import { CartaoService } from './cartao.service';
+
+const ackErrors: string[] = ['E11000'];
 
 @Controller('cartao')
 export class CartaoController {
   constructor(private readonly cartaoService: CartaoService) {}
 
   @MessagePattern('listar-cartoes')
-  async listarCartoes(email: string) {
-    return this.cartaoService.listarCartoes(email);
+  async listarCartoes(email: string, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      return this.cartaoService.listarCartoes(email);
+    } finally {
+      await channel.ack(originalMsg);
+    }
   }
 
   @EventPattern('delete-cartao')
-  async deleteCartao(idCartao: string) {
-    await this.cartaoService.deletarCartao(idCartao);
+  async deleteCartao(idCartao: string, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      await this.cartaoService.deletarCartao(idCartao);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+
+      if (filterAckError.length > 0) await channel.ack(originalMsg);
+    }
   }
 }
--- src/pagamento/pagamento.controller.ts
+++ src/pagamento/pagamento.controller.ts
@@ -1,21 +1,50 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, EventPattern } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  RmqContext,
+} from '@nestjs/microservices';
 
 import { CriarPagamentoDto } from './dto/criar-pagamento.dto';
 import { PagamentoService } from './pagamento.service';
+
+const ackErrors: string[] = ['E11000'];
 
 @Controller()
 export class PagamentoController {
   constructor(private readonly pagamentoService: PagamentoService) {}
 
   @MessagePattern('criar-intencao-de-pagamento')
-  async criarIntencaoDePagamento(data: CriarPagamentoDto) {
-    return this.pagamentoService.criarIntencaoDePagamento(data);
+  async criarIntencaoDePagamento(
+    data: CriarPagamentoDto,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      return this.pagamentoService.criarIntencaoDePagamento(data);
+    } finally {
+      await channel.ack(originalMsg);
+    }
   }
 
   @EventPattern('estornar-pagamento')
-  async estornarPagamento(idPagamento: string) {
-    return this.pagamentoService.estornarPagamento(idPagamento);
+  async estornarPagamento(idPagamento: string, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      await this.pagamentoService.estornarPagamento(idPagamento);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+
+      if (filterAckError.length > 0) await channel.ack(originalMsg);
+    }
   }
 
   @MessagePattern('buscar-publishable-key')
